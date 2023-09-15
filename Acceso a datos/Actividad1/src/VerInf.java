eea1e5628bd1e69525b7cd171115f2abd2b4d78d
Bárbara <barbara.lopez@ikasle.egibide.org>
2023-09-15 21:44:02 +0200

    ejercicios

--- Acceso a datos/Actividad1/src/VerInf.java
+++ Acceso a datos/Actividad1/src/VerInf.java
@@ -1,9 +1,10 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class VerInf {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, EOFException{
 
-        System.out.println("Información de este fichero:");
+       /* System.out.println("Información de este fichero:");
         File f = new File("src/VerInf.java");
         if (f.exists()){
             System.out.println("El nombre: " + f.getName());
@@ -27,6 +28,86 @@ public class VerInf {
             }
 
         }
+        File fichero = new File("C:\\Users\\9fdam03\\Desktop\\Clase\\DAM3-\\Acceso a datos\\Actividad1\\src\\fichero");//declarar fichero
+        FileReader fic = new FileReader(fichero); //crear el flujo de entrada
+        int i;
+        while ((i = fic.read()) != -1) //se va leyendo un carácter
+            System.out.println((char) i);
+        fic.close(); //cerrar flujo de entrada
+
+
+        File fis = new File("C:\\Users\\9fdam03\\Desktop\\Clase\\DAM3-\\Acceso a datos\\Actividad1\\src\\fichero");//declarar fichero
+        FileReader isr = new FileReader(fis);
+        char[] cbuf = new char[10];
+        // lee los datos en el buffer
+        i = isr.read(cbuf, 2, 5);
+        for(char c:cbuf)
+        {
+        // caracteres vacios
+            if(((int)c)==0)
+                c='-';
+            System.out.println(c);
+        }
+
+        try {
+            FileReader fr = new FileReader("C:\\Users\\9fdam03\\Desktop\\Clase\\DAM3-\\Acceso a datos\\Actividad1\\src\\fichero");
+            BufferedReader br = new BufferedReader(fr);
+
+            String linea;
+
+            while((linea = br.readLine())!= null)
+                System.out.println(linea);
+
+            br.close();
+        }
+        catch (FileNotFoundException fn) {
+            System.out.println("Error de lectura");
+        }
+
+        catch (IOException io){
+            System.out.println("Error de E/S");
+        }
+        String texto = "hola mundo!";
+        FileWriter f = new FileWriter ("C:\\Users\\9fdam03\\Desktop\\Clase\\DAM3-\\Acceso a datos\\Actividad1\\src\\fichero2.txt", true);
+        char[] array= texto.toCharArray();
+        int i=0;
+        while(i != array.length ) {
+            f.append(array[i]);
+            i++;
+        }
+        f.close();
+
+        String prov[]={"gipuzkoa","bizkaia","araba"};
+        FileWriter f = new FileWriter ("C:\\Users\\9fdam03\\Desktop\\Clase\\DAM3-\\Acceso a datos\\Actividad1\\src\\fichero1");
+        int i=0;
+        while(i != prov.length ) {
+            f.write(prov[i]);
+            i++;
+        }
+        f.close();
+
+        File fichero = new File("C:\\Users\\9fdam03\\Desktop\\Clase\\DAM3-\\Acceso a datos\\Actividad1\\src\\FichData.dat");
+        FileOutputStream fileout = new FileOutputStream(fichero);
+        DataOutputStream dataOS = new DataOutputStream(fileout);
+        String nombres[] = {"Ana","Luis, Miguel","Alicia","Pedro","Manuel","Andrés",
+                "Julio","Antonio","María Jesús"};
+        int edades[] = {14,15,13,15,16,12,16,14,13};
+        for (int i=0;i<edades.length; i++){
+            dataOS.writeUTF(nombres[i]); //inserta nombre
+            dataOS.writeInt(edades[i]); //inserta edad
+        }
+        dataOS.close();
+        */
+        File fichero = new File("C:\\Users\\9fdam03\\Desktop\\Clase\\DAM3-\\Acceso a datos\\Actividad1\\src\\FichData.dat");
+        FileInputStream fileout = new FileInputStream(fichero);
+        DataInputStream dataOS = new DataInputStream(fileout);
+
+        while (dataOS.available() != -1) { //lee datos del flujo de entrada
+            System.out.println(dataOS.readUTF()+ ", " + dataOS.readInt());
+
+        }
+        dataOS.close(); //cerrar stream de entrada
+
     }
 
 }
