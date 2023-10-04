4d2df340f7cc8c8ae3c3d9373c41931f78f8647c
Bárbara <barbara.lopez@ikasle.egibide.org>
2023-10-04 15:08:55 +0200

    sql

--- Acceso a datos/practica202_sql.sql
+++ Acceso a datos/practica202_sql.sql
@@ -10,5 +10,22 @@ CREATE TABLE doctors (
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+CREATE TABLE Patient (
+	id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	name varchar(45) DEFAULT NULL,
+	lastname varchar(45) DEFAULT NULL,
+    dni varchar(45) DEFAULT NULL,
+    age integer DEFAULT NULL,
+    phone varchar(45) DEFAULT NULL,
+    disease varchar(45) DEFAULT NULL,
+    doctor MEDIUMINT NOT NULL,
+	PRIMARY KEY (`id`),
+    FOREIGN KEY (doctor) REFERENCES doctors(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+insert into Patient(name,lastname,dni,age,phone,disease,doctor) values ("marta","sanchez","19215621E",12,123456789,"dolor estomacal",5);
 delete from practica202.doctors where id=2;
 INSERT INTO doctors ( `name`, `lastname`,dni,salary,speciality) VALUES ("pepe","lopez","19213239R",1500.0,"dermatolocia");
+select * from practica202.doctors;
