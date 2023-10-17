a57b387b96c6f85cf2c2307c5f0dacd6646ec19d
Bárbara <barbara.lopez@ikasle.egibide.org>
2023-10-17 21:46:55 +0200

    ejercicio1

--- PSP/ejercicios_tema3/src/Cliente.java
+++ PSP/ejercicios_tema3/src/Cliente.java
@@ -1,6 +1,9 @@
-public class Cliente {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.Socket;
 
+public class Cliente {
+    public static void main(String[] args) throws IOException {
+        Socket cliente = new Socket("localhost",4999);
 
     }
 }
--- PSP/ejercicios_tema3/src/Servidor.java
+++ PSP/ejercicios_tema3/src/Servidor.java
@@ -1,6 +1,22 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ServerSocket servidor = new ServerSocket(4999);
+        int numCli;
+        for (numCli = 1; numCli < 6; numCli++) {
+            Socket cliente = servidor.accept();
+            System.out.println("Atiendo al cliente"+numCli);
+            OutputStream aux=cliente.getOutputStream();
+            DataOutputStream flujo=new DataOutputStream(aux);
+            flujo.writeUTF("hola cliente"+numCli);
+            flujo.close();
+        }
 
 
-    } 
+    }
 }
